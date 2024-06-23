1d29b89d7d0e1541a2924e62812c09542bd3991a
Anežka Havránková <anezkahavrankova@anezka-macbook-air.home>
2024-06-24 01:25:39 +0200

    cus

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: anezkahavrankova <anezkahavrankova@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 00:04:03 by anezkahavra       #+#    #+#             */
-/*   Updated: 2024/06/24 00:57:22 by anezkahavra      ###   ########.fr       */
+/*   Updated: 2024/06/24 01:25:12 by anezkahavra      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,29 @@
 
 int	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
 {
-	int	sizeofdst;
-	int	sizeofsrc;
 	int	i;
-	char *fin;
+	int	total;
 
 	i = 0;
-	sizeofdst = ft_strlen(dst);
-	sizeofsrc = ft_strlen(src);
-	fin = dst;
-	while (sizeofdst + i <= dstsize - 1 && src[i] != '\0')
-	{
-		
-		fin[sizeofdst + i] = src[i];
-		i++;
-	}
-	fin[i] = '\0';
-	sizeofdst = ft_strlen(fin);
-	return (sizeofdst);
+	total = ft_strlen(dst) + ft_strlen(src);
+	if (total >= dstsize)
+		total = dstsize;
+	if (total < dstsize)
+		total = total + 1;
+	while (i < dstsize - 1 && src[i] != '\0')
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	return (total);
 }
 
 int main(void)
 {
     size_t dstsize = 15;
     char *dst = "hjakj";
-    char *src = "kjabvavnav";
+    char *src = "kjab";
     printf("%d", ft_strlcat(dst, src, dstsize));
     return (0);
 }
